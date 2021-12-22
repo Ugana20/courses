aadb75b62130552d5a51417f1123e9d455072ae1
lab_1_14 <lab_1_14@lab-1-14s-iMac.local>
2021-12-22 12:01:04 +0800

    Updates

---
+++ precache-manifest.55adb53d075548cb4d72a711780ffe7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "102c9a470d55ba7e1230",
+    "url": "/Ugana20/courses.git/static/css/main.dc71daae.chunk.css"
+  },
+  {
+    "revision": "102c9a470d55ba7e1230",
+    "url": "/Ugana20/courses.git/static/js/main.102c9a47.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/Ugana20/courses.git/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "7ee8fe707d5216ffaa72",
+    "url": "/Ugana20/courses.git/static/js/runtime~main.7ee8fe70.js"
+  },
+  {
+    "revision": "59870b5b6c70f073044555e06f79e7bd",
+    "url": "/Ugana20/courses.git/index.html"
+  }
+];
